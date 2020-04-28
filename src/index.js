85429523d1dee08f4eb952d24cfae2c2882396da
Drake Wilcox <drakewilcox@gmail.com>
2020-04-28 14:17:27 -0700

    add import statements for reducers and redux.
    Co-authored-by: jeremy kale padot <jeremy@padot.us>

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers/post-list-reducer';
 
 ReactDOM.render(
   <React.StrictMode>
